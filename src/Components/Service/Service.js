b3e131bfaa9f95fcc9e3d013a70ea719b9f8da9d
Pobarul-islam <pobarul3@gmail.com>
2022-07-28 10:41:07 +0600

    service card added

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Service = () => {
+const Service = ({ service }) => {
+    const {name, img, description } = service;
     return (
         <div>
-            <h3>This is service</h3>
+          
+            <div class="card w-96 bg-base-100 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img src={img} alt="Shoes" class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+                    <h2 class="card-title">{ name}</h2>
+                    <p>{ description}</p>
+    <div class="card-actions">
+      <button class="btn btn-primary">More</button>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-
+import Service from '../Service/Service';
+import './Services.css'
 const Services = () => {
     const [services, setService ] = useState([])
     useEffect(() => {
@@ -8,8 +9,18 @@ const Services = () => {
         .then(data=>setService(data))
     },[])
     return (
-        <div>
-            <h1>This is Service section {services.length}</h1>
+        <div className='mt-10 mb-5'>
+            <h2 className='text-center text-4xl font-bold'>Services</h2>
+            <div className='service-container'>
+            {
+                services.map(service => <Service
+                    key={service}
+                    service={service}
+                >
+                    
+                </Service>)
+            }
+           </div>
         </div>
     );
 };
